911eb21c202d232f60c202aa94d998c281b215e3
sh3ngsh3ng <yshenglim13@gmail.com>
2022-04-06 09:29:23 +0000

    s

--- experiment/src/app/components/propertybindingtest/propertybindingtest.component.ts
+++ experiment/src/app/components/propertybindingtest/propertybindingtest.component.ts
@@ -5,6 +5,9 @@ import { Component, OnInit } from '@angular/core';
   template: `
     <h1 [ngClass]="textStyle">Demo 1</h1>
     <h1 [class.red]="classRed">Demo 2</h1>
+    <h1 [style.color]="check ? 'green' : 'red'">Demo 3</h1>
+    <h1 [ngStyle]="allStyles">Demo 4</h1>
+    <button [value]="" (click)="buttonClicked($event)">Click this button</button>
   `,
   styles: [`
     .green {
@@ -29,6 +32,21 @@ export class PropertybindingtestComponent implements OnInit {
     "txt": true
   }
 
+  public check = false;
+
+  public allStyles = {
+    color: "yellow",
+    backgroundColor: "black"
+  }
+
+  public msg = ""
+
+  buttonClicked(event:MouseEvent) {
+    console.log("Button Clicked");
+    console.log(event);
+    // event.target.valu = "Button Clicked";
+    this.msg = "Button is clicked";
+  }
 
   constructor() { }
 
