a1d014c4f84162363f754700d8224ac0bbfdf2ec
sh3ngsh3ng <yshenglim13@gmail.com>
2022-04-06 08:07:00 +0000

    s

--- experiment/src/app/components/propertybindingtest/propertybindingtest.component.ts
+++ experiment/src/app/components/propertybindingtest/propertybindingtest.component.ts
@@ -2,10 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-propertybindingtest',
-  templateUrl: './propertybindingtest.component.html',
-  styleUrls: ['./propertybindingtest.component.css']
+  template: `
+    <h1 [ngClass]="textStyle">Demo 1</h1>
+    <h1 [class.red]="classRed">Demo 2</h1>
+  `,
+  styles: [`
+    .green {
+      color: green;
+    }
+    .red {
+      color: red
+    }
+    .txt {
+      font-style: italic;
+    }
+
+  `]
 })
 export class PropertybindingtestComponent implements OnInit {
+
+  public classGreen = "green";
+  public classRed = true;
+  public textStyle = {
+    "green": true,
+    "red": false,
+    "txt": true
+  }
+
 
   constructor() { }
 
