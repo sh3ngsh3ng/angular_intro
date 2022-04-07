a2f58701e0c9ef0b0acc66c95dd5d9889c9d84b7
sh3ngsh3ng <yshenglim13@gmail.com>
2022-04-07 07:58:48 +0000

    s

--- experiment/src/app/components/structuraldirectives/structuraldirectives.component.ts
+++ experiment/src/app/components/structuraldirectives/structuraldirectives.component.ts
@@ -15,15 +15,22 @@ export class StructuraldirectivesComponent implements OnInit {
   // *ngIf
   flag = true
   displayText() {
-    this.flag = !this.flag
+    this.flag = !this.flag;
   }
+
+  flag2 = true
+  displayText2() {
+    this.flag2 = !this.flag2;
+  }
+
 
   // switch case
   // *ngSwitch
-
+  public day = "tues";
 
   // for loop
   // *ngFor
+  public participants = ["Man 1", "Woman 2", "Child 3", "God 4"]
 
  
 
